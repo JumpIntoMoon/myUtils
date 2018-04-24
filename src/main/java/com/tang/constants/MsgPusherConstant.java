8a6b754aec90fa2791ae7a6f878ce0466d84e73c
tangyilong <t_yl@outlook.com>
2018-04-24 10:15:47 +0800

    添加 websocket 消息推送模块 该模块实现web工程前后端消息推送功能

---
+++ src/main/java/com/tang/constants/MsgPusherConstant.java
@@ -0,0 +1,11 @@
+package com.tang.constants;
+
+/**
+ * @description: 消息推送用到的一些常量
+ * @author: tangYiLong
+ * @create: 2018-04-24 10:00
+ **/
+public class MsgPusherConstant {
+    //websocket 用户标识
+    public static final String WEBSOCKET_CLIENT_ID = "userId";
+}
